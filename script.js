395d43e0ec7a1ba5c06efb45e190d117380f046a
SayeemZaman3 <sayeem312@gmail.com>
2025-06-21 18:43:25 +0600

    add pricing for monthly/yearly

--- script.js
+++ script.js
@@ -68,3 +68,57 @@ function updateCrumbs() {
 $('form').submit((e) => {
   e.preventDefault();
 })
+
+// Monthly & Yearly
+let prices = {
+  monthly: {
+    plan: {
+      arcade: '9',
+      advanced: '12',
+      pro: '15'
+    },
+    addons: {
+      addon1: '1',
+      addon2: '2',
+      addon3: '2'
+    },
+  },
+  yearly: {
+    plan: {
+      arcade: '90',
+      advanced: '120',
+      pro: '150'
+    },
+    addons: {
+      addon1: '10',
+      addon2: '20',
+      addon3: '20'
+    },
+  }
+};
+
+// Checked = Yearly, Unchecked = Monthly
+$(document).click(typeCheck);
+
+function typeCheck(){
+  $('.switch input').is(':checked')
+    ? setPrice('yearly') : setPrice('monthly');
+}
+typeCheck()
+
+function setPrice(type) {
+  // Plan
+  $('#arcadePrice').text(`+${prices[type].plan.arcade}/mo`);
+  $('#advancedPrice').text(`+${prices[type].plan.advanced}/mo`);
+  $('#proPrice').text(`+${prices[type].plan.pro}/mo`);
+  
+  // Discount
+  type === 'yearly' ?
+  $('.free').addClass('active') :
+  $('.free').removeClass('active');
+  
+  // Add-ons
+  $('#addon1-price').text(`+${prices[type].addons.addon1}/mo`);
+  $('#addon2-price').text(`+${prices[type].addons.addon2}/mo`);
+  $('#addon3-price').text(`+${prices[type].addons.addon3}/mo`);
+}
